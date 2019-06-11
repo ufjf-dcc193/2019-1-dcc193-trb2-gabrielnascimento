cef99d3e43026c64045ac03bfff1f3258039a8c2
gabrielsantosnt <gabrieljf13@gmail.com>
2019-06-11 20:13:09 -0300

    view avaliador form

--- src/main/java/ufjf/dcc193/tr2/DemoApplication.java
+++ src/main/java/ufjf/dcc193/tr2/DemoApplication.java
@@ -1,13 +1,36 @@
 package ufjf.dcc193.tr2;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import ufjf.dcc193.tr2.dao.AreaConhecimentoRepository;
+import ufjf.dcc193.tr2.model.AreaConhecimento;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Autowired
+	AreaConhecimentoRepository areaConhecimentoRep;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	InitializingBean sendDatabase() {
+		return () -> {
+			areaConhecimentoRep.save(new AreaConhecimento(null, "Exatas"));
+			areaConhecimentoRep.save(new AreaConhecimento(null, "Biológicas"));
+			areaConhecimentoRep.save(new AreaConhecimento(null, "Engenharias"));
+			areaConhecimentoRep.save(new AreaConhecimento(null, "Saúde"));
+			areaConhecimentoRep.save(new AreaConhecimento(null, "Socias Aplicadas"));
+			areaConhecimentoRep.save(new AreaConhecimento(null, "Humanas"));
+			areaConhecimentoRep.save(new AreaConhecimento(null, "Linguística, Letras e Artes"));
+
+		};
 	}
 
 }
