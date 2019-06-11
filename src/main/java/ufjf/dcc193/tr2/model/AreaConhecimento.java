6fd21829bd0a0b2e9afc35c72adaaf8cee08b529
gabrielnascimento95 <gabriel.nascimento@ice.ufjf.br>
2019-06-11 19:11:04 -0300

    criação do model area e avaliador

---
+++ src/main/java/ufjf/dcc193/tr2/model/AreaConhecimento.java
@@ -0,0 +1,59 @@
+package ufjf.dcc193.tr2.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+/**
+ * AreaConhecimento
+ */
+@Entity
+public class AreaConhecimento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String descricao;
+
+    public AreaConhecimento() {
+    }
+
+    public AreaConhecimento(Long id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public AreaConhecimento id(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public AreaConhecimento descricao(String descricao) {
+        this.descricao = descricao;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "{id='" + getId() + "'" + ", descricao='" + getDescricao() + "'}";
+    }
+}
---
+++ src/main/java/ufjf/dcc193/tr2/model/Avaliador.java
@@ -0,0 +1,85 @@
+package ufjf.dcc193.tr2.model;
+
+import java.util.List;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Entity;
+
+/**
+ * Avaliador
+ */
+@Entity
+public class Avaliador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String nome;
+    private String email;
+    private String codigoAcesso;
+
+    @ManyToMany
+    private List<AreaConhecimento> areaConhecimento;
+
+    public Avaliador() {
+    }
+
+    public Avaliador(Long id, String nome, String email, String codigoAcesso, List<AreaConhecimento> areaConhecimento) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.codigoAcesso = codigoAcesso;
+        this.areaConhecimento = areaConhecimento;
+
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCodigoAcesso() {
+        return this.codigoAcesso;
+    }
+
+    public void setCodigoAcesso(String codigoAcesso) {
+        this.codigoAcesso = codigoAcesso;
+    }
+
+    public List<AreaConhecimento> getAreaConhecimento() {
+        return this.areaConhecimento;
+    }
+
+    public void setAreaConhecimento(List<AreaConhecimento> areaConhecimento) {
+        this.areaConhecimento = areaConhecimento;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + " id='" + getId() + "'" + ", nome='" + getNome() + "'" + ", email='" + getEmail() + "'"
+                + ", codigoAcesso='" + getCodigoAcesso() + "'" + ", areaConhecimento='" + getAreaConhecimento() + "'"
+                + "}";
+    }
+}
