4abb91f4bb065428217d5dac662bca143f6d6ba4
gabrielsantosnt <gabrieljf13@gmail.com>
2019-06-13 19:06:55 -0300

    trabalho form

--- src/main/java/ufjf/dcc193/tr2/controller/TrabalhoController.java
+++ src/main/java/ufjf/dcc193/tr2/controller/TrabalhoController.java
@@ -3,6 +3,7 @@ package ufjf.dcc193.tr2.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ufjf.dcc193.tr2.dao.AreaConhecimentoRepository;
@@ -38,6 +39,25 @@ public class TrabalhoController {
     @RequestMapping("/salvar")
     public String salvarSubmit(Trabalho trabalho){
         repTrabalho.save(trabalho);
+        return "redirect:/trabalho/";
+    }
+
+    @RequestMapping("/editar/{id}")
+    public String preEditarTrabalho(@PathVariable Long id, Model model){
+        model.addAttribute("trabalho",repTrabalho.findById(id).get());
+        model.addAttribute("listaArea",repAreaConhecimento.findAll());
+        return "trabalho/trabalho-edit-form";
+    }
+
+    @RequestMapping("/editar/salvar")
+    public String editarTrabalhoSubmit(Trabalho trabalho){
+        repTrabalho.save(trabalho);
+        return "redirect:/trabalho/";
+    }
+
+    @RequestMapping("/deletar/{id}")
+    public String deletarTrabalho(@PathVariable Long id){
+        repTrabalho.deleteById(id);
         return "redirect:/trabalho/";
     }
 }
